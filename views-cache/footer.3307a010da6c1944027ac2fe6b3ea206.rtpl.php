182261c4b7d5dbb2cd2879a55a32275aa62dfa85
Adriano Mantovani Armelim <aarmelim@hotmail.com>
2020-07-17 16:27:04 -0300

    separação do header, footer e main em arquivos

--- views-cache/footer.3307a010da6c1944027ac2fe6b3ea206.rtpl.php
+++ views-cache/footer.3307a010da6c1944027ac2fe6b3ea206.rtpl.php
@@ -1,2 +1,98 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?>    </body>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="footer-top-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-about-us">
+                    <h2>Anna K Store</h2>
+                    <p>Oferecemos trabalho de Consultoria de Moda e Imagem focando nas necessidades pessoais e formato visual. Criamos looks que se encaixam e atendam todas as necessidades e desejos das pessoas.</p>
+                    <div class="footer-social">
+                        <a href="https://www.instagram.com/anna_k_store" target="_blank"><i class="fa fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-menu">
+                    <h2 class="footer-wid-title">Navegação </h2>
+                    <ul>
+                        <li><a href="#">Minha Conta</a></li>
+                        <li><a href="#">Meus Pedidos</a></li>
+                        <li><a href="#">Lista de Desejos</a></li>
+                    </ul>                        
+                </div>
+            </div>
+            
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-menu">
+                    <h2 class="footer-wid-title">Categorias</h2>
+                    <ul>
+                        <li><a href="#">Categoria Um</a></li>
+                        <li><a href="#">Categoria Dois</a></li>
+                        <li><a href="#">Categoria Três</a></li>
+                        <li><a href="#">Categoria Quarto</a></li>
+                        <li><a href="#">Categoria Cinco</a></li>
+                    </ul>                        
+                </div>
+            </div>
+            
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-newsletter">
+                    <h2 class="footer-wid-title">Newsletter</h2>
+                    <p>Se quiser receber nossas novidades deixe-nos seu email</p>
+                    <div class="newsletter-form">
+                        <form action="#">
+                            <input type="email" placeholder="Type your email">
+                            <input type="submit" value="Subscribe">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End footer top area -->
+
+<div class="footer-bottom-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="copyright">
+                    <p>&copy; 2020 Anna K Store. <a href="http://www.annakstore.com.br" target="_blank">annakstore.com.br</a></p>
+                </div>
+            </div>
+            
+            <div class="col-md-2">
+                <div class="footer-card-icon">
+                    <i class="fa fa-cc-paypal"></i>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="logo">
+                    <a href="#"><img src="https://assets.pagseguro.com.br/ps-bootstrap/v6.71.0/img/logos/pagbank/pagbank-logo-animado_30px@2x.gif"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End footer bottom area -->
+
+<!-- Latest jQuery form server -->
+<script src="https://code.jquery.com/jquery.min.js"></script>
+
+<!-- Bootstrap JS form CDN -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+<!-- jQuery sticky menu -->
+<script src="res/site/js/owl.carousel.min.js"></script>
+<script src="res/site/js/jquery.sticky.js"></script>
+
+<!-- jQuery easing -->
+<script src="res/site/js/jquery.easing.1.3.min.js"></script>
+
+<!-- Main Script -->
+<script src="res/site/js/main.js"></script>
+
+<!-- Slider -->
+<script type="text/javascript" src="res/site/js/bxslider.min.js"></script>
+<script type="text/javascript" src="res/site/js/script.slider.js"></script>
+</body>
 </html>
