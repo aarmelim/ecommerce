e343e0ea09eda0e83268490d29d3c72e2af25c96
Adriano Mantovani Armelim <aarmelim@hotmail.com>
2020-07-20 15:39:06 -0300

    colcoar a categoria no site

--- views-cache/header.3307a010da6c1944027ac2fe6b3ea206.rtpl.php
+++ views-cache/header.3307a010da6c1944027ac2fe6b3ea206.rtpl.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="res/site/img/logo1.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/res/site/img/logo1.png" type="image/x-icon">
     <title>Anna K. Store</title>
     
     <!-- Google Fonts -->
@@ -16,15 +16,15 @@
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
     
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="res/site/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/res/site/css/bootstrap.min.css">
     
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="res/site/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/res/site/css/font-awesome.min.css">
     
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="res/site/css/owl.carousel.css">
-    <link rel="stylesheet" href="res/site/css/style.css">
-    <link rel="stylesheet" href="res/site/css/responsive.css">
+    <link rel="stylesheet" href="/res/site/css/owl.carousel.css">
+    <link rel="stylesheet" href="/res/site/css/style.css">
+    <link rel="stylesheet" href="/res/site/css/responsive.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -80,7 +80,7 @@
             <div class="row">
                 <div class="col-sm-6">
                     <div class="logo">
-                        <h1><a href="#"><img src="res/site/img/logo.png"></a></h1>
+                        <h1><a href="#"><img src="/res/site/img/logo.png"></a></h1>
                     </div>
                 </div>
                 
