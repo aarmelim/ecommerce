2888cc29f1203a66fefc5f50653ea91c8a34f26b
Adriano Mantovani Armelim <aarmelim@hotmail.com>
2020-08-02 11:41:52 -0300

    criação da classe holder

--- vendor/aarmelim/php-classes/src/PagSeguro/Credicard/Holder.php
+++ vendor/aarmelim/php-classes/src/PagSeguro/Credicard/Holder.php
@@ -1,8 +1,15 @@
 <?php
 
-//nome de quem está pagando...
+//nome de quem está pagando... do dono do cartão de crédito
 
 namespace aarmelim\PagSeguro\CreditCard;
+
+use Exception;
+use DOMDocument;
+use DOMElement;
+use DateTime;
+use aarmelim\PagSeguro\Document;
+use aarmelim\PagSeguro\Phone;
 
 class Holder {
 
@@ -10,5 +17,57 @@ class Holder {
 	private $cpf;
 	private $birthDate;
     private $phone;
-    
+   
+	public function __construct(
+		string $name,
+		Document $cpf,
+		DateTime $birthDate,
+		Phone $phone
+	)
+	{
+
+		if (!$name)
+		{
+
+			throw new Exception("Informe o nome do comprador.");
+
+		}
+
+		$this->name = $name;
+		$this->cpf = $cpf;
+		$this->birthDate = $birthDate;
+		$this->phone = $phone;
+
+	}
+
+	public function getDOMElement()
+	{
+	
+		$dom = new DOMDocument();
+
+		$holder = $dom->createElement("holder");
+		$holder = $dom->appendChild($holder);
+
+		$name = $dom->createElement("name", $this->name);
+		$name = $holder->appendChild($name);
+
+		$birthDate = $dom->createElement("birthDate", $this->birthDate->format("d/m/Y"));
+		$birthDate = $holder->appendChild($birthDate);
+
+		$documents = $dom->createElement("documents");
+		$documents = $holder->appendChild($documents);
+		
+		$cpf = $this->cpf->getDomElement();
+		$cpf = $dom->importNode($cpf, true);
+		$cpf = $documents->appendChild($cpf);
+
+		$phone = $this->phone->getDomElement();
+		$phone = $dom->importNode($phone, true);
+		$phone = $holder->appendChild($phone);
+
+		return $holder;
+
+	}
+
+
 }
