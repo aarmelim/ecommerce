5fb594d0821afe338d1630e1f2c8133f7e98aff6
Adriano Mantovani Armelim <aarmelim@hotmail.com>
2020-08-02 11:47:52 -0300

    criação da classe shipping

--- vendor/aarmelim/php-classes/src/PagSeguro/Shipping.php
+++ vendor/aarmelim/php-classes/src/PagSeguro/Shipping.php
@@ -2,6 +2,10 @@
 
 namespace aarmelim\PagSeguro;
 
+use Exception;
+use DOMDocument;
+use DOMElement;
+
 class Shipping {
 
 	const PAC = 1;
@@ -12,5 +16,52 @@ class Shipping {
 	private $type;
 	private $cost;
     private $addressRequired;
-    
+	
+	public function __construct(
+		Address $address,
+		float $cost,
+		int $type,		
+		bool $addressRequired = true //para saber se o endereço é obrigatório ou não. nem todos os tipos exigem endereço. para venda de serviço por exemplo.
+	)
+	{
+
+		if ($type < 1 || $type > 3)
+		{
+
+			throw new Exception("Informe um tipo de frete válido");
+
+		}
+
+		$this->address = $address;
+		$this->cost = $cost;
+		$this->type = $type;
+		$this->addressRequired = $addressRequired;
+
+	}
+
+	public function getDOMElement()
+	{
+	
+		$dom = new DOMDocument();
+
+		$shipping = $dom->createElement("shipping");
+		$shipping = $dom->appendChild($shipping);
+
+		$address = $this->address->getDomElement();
+		$address = $dom->importNode($address, true);
+		$address = $shipping->appendChild($address);
+
+		$cost = $dom->createElement("cost", number_format($this->cost, 2, ".", ""));
+		$cost = $shipping->appendChild($cost);
+
+		$type = $dom->createElement("type", $this->type);
+		$type = $shipping->appendChild($type);
+
+		$addressRequired = $dom->createElement("addressRequired", ($this->addressRequired) ? "true" : "false");
+		$addressRequired = $shipping->appendChild($addressRequired);
+
+		return $shipping;
+
+	}
+
 }
