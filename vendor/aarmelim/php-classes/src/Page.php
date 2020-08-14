793933608b8c9a49e85385c7b5dc34f1d9b1a8fe
Adriano Mantovani Armelim <aarmelim@hotmail.com>
2020-08-14 16:57:20 -0300

    fazer funcionar na locaweb em produção

--- vendor/aarmelim/php-classes/src/Page.php
+++ vendor/aarmelim/php-classes/src/Page.php
@@ -14,13 +14,13 @@ class Page {
         "data"=>[]
     ];
 
-    public function __construct($opts = array(), $tpl_dir = "/views/"){
+    public function __construct($opts = array(), $tpl_dir = DIRECTORY_SEPARATOR . "clientes" . DIRECTORY_SEPARATOR . "annakstore" . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR){
         
         $this->options = array_merge($this->defaults, $opts);
 
         $config = array(
             "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
-            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "clientes" . DIRECTORY_SEPARATOR . "annakstore" . DIRECTORY_SEPARATOR . "views-cache" . DIRECTORY_SEPARATOR,
             "debug"         => true // set to false to improve the speed
         );
 
